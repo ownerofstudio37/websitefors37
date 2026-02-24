55fc379ef4686c11a2cdb7aa4941f4a5b4203653
Studio37 <ceostudio37.cc@Studio37s-MacBook-Pro.local>
2026-02-23 22:19:11 -0600

    fix: allow api sitemap in robots

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin/', '/api/'],
+      disallow: ['/admin/'],
     },
     sitemap: `${businessInfo.contact.website}/sitemap.xml`,
   }
