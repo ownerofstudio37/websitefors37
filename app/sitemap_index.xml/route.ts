bf1cac02977871c31d7c755aebb458baaa93e43c
ownerofstudio37 <ceo@studio37.cc>
2026-06-03 00:17:46 -0500

    ff

---
+++ app/sitemap_index.xml/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+
+export const revalidate = 1800
+
+export async function GET() {
+  const baseUrl = 'https://www.studio37.cc'
+  const now = new Date().toISOString()
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${baseUrl}/sitemap.xml</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/sitemap-static.xml</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+</sitemapindex>
+`
+
+  return new NextResponse(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=1800',
+    },
+  })
+}
